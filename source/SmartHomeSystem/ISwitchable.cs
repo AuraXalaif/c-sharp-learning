109470cefbd54c4ae42f6b277585d3b139846418
AuraXalaiv <epikfail94@gmail.com>
2025-12-03 16:13:08 +0200

    add source/

---
+++ source/SmartHomeSystem/ISwitchable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSystem
+{
+    public interface ISwitchable
+    {
+        bool IsOn { get; }
+
+        void TurnOn();
+
+        void TurnOff();
+    }
+}
---
+++ source/SmartHomeSystem/Light.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SmartHomeSystem
+{
+    public class Light : Device, IEnergyConsumer
+    {
+        public string DeviceName => Name;
+        public int PowerConsumption => 60;
+
+        public override void TurnOn()
+        {
+            if (!IsOn)
+            {
+                IsOn = true;
+                Console.WriteLine($"{Name} засвітилася.");
+            }
+        }
+
+        public override void TurnOff()
+        {
+            if (IsOn)
+            {
+                IsOn = false;
+                Console.WriteLine($"{Name} вимкнена.");
+            }
+        }
+
+        public double GetEnergyUsage(int hours)
+        {
+            if (IsOn)
+                return PowerConsumption * hours / 1000.0;
+            else
+                return 0;
+        }
+    }
+}
